81f66f9241dfc8f70bfdfd24febf14ed7a8541aa
Davi Nascimento <davi-nasc@hotmail.com>
2017-10-13 12:53:38 -0300

    Add campo admin no model user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
-  # user = 0  client = 1 salesman = 2 guide = 3
-  enum user_type: [:user, :manager, :salesman, :guide]
+  # user = 0  client = 1 salesman = 2 guide = 3 manager = 4
+  enum user_type: [:user, :manager, :salesman, :guide, :user_manager]
 
   # Include default devise modules.
   devise :database_authenticatable, :registerable,
