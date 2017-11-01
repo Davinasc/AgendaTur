c5e35ef09624f93686edfbfad2fea64a13806289
Davi Nascimento <davi-nasc@hotmail.com>
2017-10-31 22:09:27 -0300

    Criado recurso cliente

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
   
+  resources :clients
+  
   resources :guides, only: [:index, :show] do
     get '/tours', to: 'guides#tours', as: 'tours'  
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171026215018) do
+ActiveRecord::Schema.define(version: 20171031223135) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "clients", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "phone"
+    t.string "hotel"
+    t.string "apartment"
+    t.boolean "adult"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "routes", force: :cascade do |t|
     t.string "name", null: false
